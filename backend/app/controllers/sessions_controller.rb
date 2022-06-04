1e7cd15cc0b53b83c23e72b90882e77bc25f9074
RoUchiha <swaggsikh@gmail.com>
2022-06-04 01:50:24 -0500

    added to sessions controllers. added helper methods to app controller.

--- backend/app/controllers/sessions_controller.rb
+++ backend/app/controllers/sessions_controller.rb
@@ -1,13 +1,18 @@
 class SessionsController < ApplicationController
 
     def create 
-        @user = User.find_by(username: params[:username])
-        if @user && @user.authenticate(params[:password])
-            session[:user_id] = @user.id 
+        @user = User.find_by(username: params["user"][:username])
+        if @user && @user.authenticate(params["user"][:password])
+            login!
             redirect_to user_path(@user)
         else
             redirect_to new_session_path
+            errors: ['Please check your username/password and try again, or create an account if you do not have one.']
         end
+    end
+
+    def destroy
+        logout!
     end
 
 end
