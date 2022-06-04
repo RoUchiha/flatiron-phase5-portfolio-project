8d02fe9388e7259d444c51ba5f3ba25a674d54fa
RoUchiha <swaggsikh@gmail.com>
2022-06-04 00:47:55 -0500

    created sessions controller for user auth.

---
+++ backend/app/controllers/sessions_controller.rb
@@ -0,0 +1,13 @@
+class SessionsController < ApplicationController
+
+    def create 
+        @user = User.find_by(username: params[:username])
+        if @user && @user.authenticate(params[:password])
+            session[:user_id] = @user.id 
+            redirect_to user_path(@user)
+        else
+            redirect_to new_session_path
+        end
+    end
+
+end
--- backend/app/models/user.rb
+++ backend/app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ApplicationRecord
 
+    has_secure_password
     has_many :teams
+    has_many :comments
 
 end
