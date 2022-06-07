521c57763fe877b86f8844738efc800926c394f5
RoUchiha <swaggsikh@gmail.com>
2022-06-06 23:57:51 -0500

    finished user login action. working on logout.

--- pokesquad/src/actions/userActions.js
+++ pokesquad/src/actions/userActions.js
@@ -26,3 +26,33 @@ export function signup(user) {
 }
 
 
+export function login(user) {
+
+    return dispatch => {
+        fetch(`/sessions`, {
+            method: "POST",
+            headers: {
+                "Access-Control-Allow-Origins": "*",
+                "Content-Type": "application/json",
+                Accept: "application/json"
+            },
+            body: JSON.stringify(user)
+        },
+            { withCredentials: true }
+        )
+        .then(response => response.json())
+        .then(data => {
+            console.log("login", data)
+            if (data.logged_in) {
+                dispatch({ type: LOGIN, payload: data })
+            }
+        })
+        .catch(error => console.log("login error", error))
+    }
+
+}
+
+
+export function logout() {
+    return dispatch
+}
