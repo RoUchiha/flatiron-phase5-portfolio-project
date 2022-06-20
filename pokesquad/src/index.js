af59a120e11da82b1e0e5113cbfb49006b91df35
RoUchiha <swaggsikh@gmail.com>
2022-06-20 04:27:20 -0500

    missed commits

--- pokesquad/src/index.js
+++ pokesquad/src/index.js
@@ -4,12 +4,12 @@ import { Provider } from "react-redux";
 import { legacy_createStore as createStore, applyMiddleware } from "redux";
 import thunkMiddleware from "redux-thunk";
 import { BrowserRouter as Router } from "react-router-dom";
-import rootReducer from "./reducers/reducer.js";
+import rootPersisted from "./reducers/reducer.js";
 import App from './App.js'
 import { composeWithDevTools } from 'redux-devtools-extension'
 
 
-const store = createStore(rootReducer, composeWithDevTools( applyMiddleware(thunkMiddleware)));
+const store = createStore(rootPersisted, composeWithDevTools( applyMiddleware(thunkMiddleware)));
 console.log(store.getState())
 
 ReactDOM.render(
